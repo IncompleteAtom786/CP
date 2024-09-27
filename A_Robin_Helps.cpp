304b30e78e84c21110334a6e8194c114f96d84aa
IncompleteAtom786 <taherkapadia786@gmail.com>
2024-09-27 19:00:09 +0530

    commit

---
+++ A_Robin_Helps.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+#define sorted(v) sort(v.begin(), v.end())
+#define ll long long
+#define loopI(i,n) for(int i = 0; i < n; i++)
+#define loopD(i,n) for(int i = n - 1; i >= 0; i--)
+
+using namespace std;
+void solve()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, k;
+        cin >> n >> k;
+
+        vector<int> gold(n);
+        int result = 0, zero = 0, robinGold = 0;
+        for(int i = 0; i < n; i++)
+        {
+            cin >> gold[i];
+            if(gold[i] == 0 && robinGold > 0) result++, robinGold--;
+            if(gold[i] >= k) robinGold += gold[i];
+        }
+
+        // result = (robinGold >= zero ? zero: robinGold - zero);
+        cout << result << endl;
+    }
+}
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    solve();
+    return 0;
+}
